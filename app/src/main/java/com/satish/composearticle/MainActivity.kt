0ab953fab6d08e713b6d7c974810e2fbaa80a7d1
satish.chanda <satish.chanda@cerence.com>
2024-10-07 01:18:51 -0400

    Added the Compose Quandrant App code. Updated the code by checking the solution.

--- app/src/main/java/com/satish/composearticle/MainActivity.kt
+++ app/src/main/java/com/satish/composearticle/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -37,8 +41,9 @@ class MainActivity : ComponentActivity() {
 //                        stringResource(id = R.string.article_information),
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
-                    TaskManager(taskStatus = stringResource(id = R.string.task_status), compliment = stringResource(
-                        id = R.string.task_compliment))
+//                    TaskManager(taskStatus = stringResource(id = R.string.task_status), compliment = stringResource(
+//                        id = R.string.task_compliment))
+                      ComposeQuadrant()
                 }
             }
         }
@@ -72,12 +77,20 @@ fun TaskManagerPreview() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantPreview() {
+    ComposeArticleTheme {
+        ComposeQuadrant()
+    }
+}
+
 
 @Composable
 fun ComposeArticle(heading: String, subHeading: String,information: String, modifier: Modifier = Modifier)  {
     val image = painterResource(id = R.drawable.bg_compose_background)
     val padding = 16.dp
-    Column(modifier) {
+    Column(modifier = modifier) {
         Image(
             painter = image,
             contentDescription = null
@@ -85,16 +98,16 @@ fun ComposeArticle(heading: String, subHeading: String,information: String, modi
         Text (
             text = heading,
             fontSize = 24.sp,
-            modifier = modifier.padding(padding)
+            modifier = Modifier.padding(padding)
         )
         Text (
             text = subHeading,
-            modifier = modifier.padding(start=padding,top=0.dp,end=padding,bottom=0.dp),
+            modifier = Modifier.padding(start=padding, end=padding),
             textAlign = TextAlign.Justify
         )
         Text (
             text = information,
-            modifier = modifier.padding(padding),
+            modifier = Modifier.padding(padding),
             textAlign = TextAlign.Justify
         )
     }
@@ -113,11 +126,67 @@ fun TaskManager(taskStatus: String, compliment: String, modifier: Modifier = Mod
         Text(
             text = taskStatus,
             fontWeight = FontWeight.Bold,
-            modifier = modifier.padding(0.dp,24.dp,0.dp,8.dp)
+            modifier = Modifier.padding(top = 24.dp,bottom = 8.dp)
         )
         Text (
             text = compliment,
             fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun ComposeQuadrant() {
+    Column(Modifier.fillMaxSize()) {
+        Row (Modifier.weight(1f)) {
+            SingleQuadrant(
+                heading = "Text Composable",
+                subHeading = "Displays text and follows the recommended Material Design guidelines.",
+                bgColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            SingleQuadrant(
+                heading = "Image Composable",
+                subHeading = "Create a composable that lays out and draws a given Painter class Object",
+                bgColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row (Modifier.weight(1f)) {
+            SingleQuadrant(
+                heading = "Row Composable",
+                subHeading = "A layout composable that places its children in a horizontal sequence.",
+                bgColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            SingleQuadrant(
+                heading = "Column Composable",
+                subHeading = "A layout composable that places its children in a vertical sequence.",
+                bgColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun SingleQuadrant(heading: String, subHeading: String, bgColor: Color,modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(bgColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = heading,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = subHeading,
+            textAlign = TextAlign.Justify
         )
     }
 }
